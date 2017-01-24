0fa98e78ab849d1f0d612e51765735f7e7626331
Ram81 <ramramrakhya81@gmail.com>
2017-01-24 19:21:59 +0530

    Building Models for dataset Done

--- iris.py
+++ iris.py
@@ -38,3 +38,55 @@ plt.show()
 #scatter plot matrix
 scatter_matrix(dataset)
 plt.show()
+
+data_arr = dataset.values;
+X = data_arr[:,0:4]
+Y = data_arr[:,4]
+validation_size = 0.20
+seed = 7
+X_Train, X_Validate, Y_Train, Y_Validate = model_selection.train_test_split(X, Y, test_size=validation_size, random_state=seed)
+
+seed = 7
+scoring='accuracy'
+
+
+#building models
+models = []
+models.append(('LR',LogisticRegression()))
+models.append(('KNN',KNeighborsClassifier()))
+models.append(('LDA',LinearDiscriminantAnalysis()))
+models.append(('CART',DecisionTreeClassifier()))
+models.append(('SVM',SVC()))
+models.append(('NB',GaussianNB()))
+
+#evaluate each model
+result = []
+names = []
+
+for name, model in models:
+	kfold = model_selection.KFold(n_splits=10, random_state = seed)
+	cv_results = model_selection.cross_val_score(model, X_Train, Y_Train, cv=kfold, scoring=scoring)
+	result.append(cv_results)
+	names.append(name)
+	msg = "%s: %f (%f)" % (name,cv_results.mean(),cv_results.std())
+	print(msg)
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
